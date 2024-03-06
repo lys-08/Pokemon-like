6686bb7e0328161b55bcb87f60a02d2dd4df9704
Chouquette <lydie.sts@gmail.com>
2024-03-06 12:18:50 +0100

    Implémentation de Wild Pokemon
    - Ajout des getters
    - Génération aléatoire des probabilités des différentes actions lors d'un combat à l'initialisation
    - Détection du player et sa position par rapport ai pokemon pour lancer le combat
    - détermination de l'objet obtenu lorsque le pokémon est KO (fonction GetObj appelé à la fin du combat si le pokemon est KO)

--- Assets/Pokemon/WildPokemon.cs
+++ Assets/Pokemon/WildPokemon.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WildPokemon : Pokemon
 {
@@ -14,17 +16,85 @@ public class WildPokemon : Pokemon
     
     private Dictionary<int, GameObject> objs_; // Possible objects that can be given when the pokemon is KO
 
+    private GameObject player_;
+
+
+    #region Getters
+
+    public float GetAttackingCoeff()
+    {
+        return attackingCoeff_;
+    }
+
+    public float GetDistractCoeff()
+    {
+        return distractCoeff_;
+    }
+
+    public float GetFocusCoeff()
+    {
+        return focusCoeff_;
+    }
+
+    #endregion
     
     
+    
+    /**
+     * Function returning the item obtained once the pokémon is KO
+     * -> Dictionary objects have a probability of optention
+     */
     public GameObject GetObj()
     {
-        // TODO
-        return objs_[0];
+        int nb = Random.Range(1, 100);
+
+        foreach (var obj in objs_)
+        {
+            if (obj.Key >= nb) return obj.Value;
+        }
+
+        // If no object matches, we return a game object null: the player does not retrieve an item
+        return null;
     }
 
 
+    /**
+     * Function that detects if the player is in the pokemon range. If he is, than a fight is launch
+     */
     private void LaunchFight()
     {
         // TODO
     }
+
+
+    /**
+     *  Function that generate the coefficients of the attack the pokemon do in a fight
+     * -> this function is called at the creation of the pokemon
+     */
+    private void GenerateCoeffs()
+    {
+        attackingCoeff_ = Random.Range(50, 70) / 100f;
+        focusCoeff_ = Random.Range(attackingCoeff_, 85) / 100f;
+        distractCoeff_ = 1f - attackingCoeff_ - focusCoeff_;
+    }
+
+
+    #region Unity Event Function
+
+    private void Awake()
+    {
+        player_ = GameObject.FindWithTag("Player");
+        GenerateCoeffs();
+    }
+
+    private void Update()
+    {
+        /*
+         * If the player is in the range of the pokemon, than a fight start
+         */
+        if (Vector3.Distance(transform.position, player_.transform.position) > 5f) return;
+        LaunchFight();
+    }
+
+    #endregion
 }
