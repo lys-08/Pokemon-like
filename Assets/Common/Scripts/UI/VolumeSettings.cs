22b3a04b7c1ed551b0fb7f0e91e449817c842b78
Chouquette <lydie.sts@gmail.com>
2024-03-11 17:48:39 +0100

    Modification de la valeur du son affiché dans les menus

--- Assets/Common/Scripts/UI/VolumeSettings.cs
+++ Assets/Common/Scripts/UI/VolumeSettings.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -12,6 +13,10 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
+    [SerializeField] private TextMeshProUGUI masterValue;
+    [SerializeField] private TextMeshProUGUI musicValue;
+    [SerializeField] private TextMeshProUGUI sfxValue;
+
     private float volume_ = 0f;
 
 
@@ -20,6 +25,8 @@ public class VolumeSettings : MonoBehaviour
         volume_ = Mathf.Log(masterSlider.value) * 20f;
         mixer.SetFloat("Master", volume_);
         PlayerPrefs.SetFloat("masterVolume", volume_);
+
+        masterValue.text = volume_.ToString();
     }
     
     public void SetMusicVolume()
@@ -27,6 +34,8 @@ public class VolumeSettings : MonoBehaviour
         volume_ = Mathf.Log(musicSlider.value) * 20f;
         mixer.SetFloat("Music", volume_);
         PlayerPrefs.SetFloat("musicVolume", volume_);
+        
+        musicValue.text = volume_.ToString();
     }
     
     public void SetSFXVolume()
@@ -34,6 +43,8 @@ public class VolumeSettings : MonoBehaviour
         volume_ = Mathf.Log(sfxSlider.value) * 20f;
         mixer.SetFloat("SFX", volume_);
         PlayerPrefs.SetFloat("sfxVolume", volume_);
+        
+        sfxValue.text = volume_.ToString();
     }
 
     public void LoadVolume()
@@ -41,6 +52,10 @@ public class VolumeSettings : MonoBehaviour
         masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        
+        masterValue.text = masterSlider.value.ToString();
+        musicValue.text = musicSlider.value.ToString();
+        sfxValue.text = sfxSlider.value.ToString();
     }
 
     private void Start()
