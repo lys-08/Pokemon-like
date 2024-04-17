56e69e8268cc78e89b42f6cf05debbe1683b0893
Chouquette <lydie.sts@gmail.com>
2024-04-17 17:35:28 +0200

    Ajout des éléments dans l'inventaire
    Fonction permettant d'augmenter la quantité d'un élément si on le récupère et que l'on en possède déjà un dans l'inventaire
    
    -> Si l'élément est plein et qu'il reste de la place dans l'inventaire, alors un nouvel éléments est créé
    -> Sinon, rien ne se passe

--- Assets/Inventory/Scripts/InventorySO.cs
+++ Assets/Inventory/Scripts/InventorySO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
@@ -32,16 +33,24 @@ namespace Inventory.Model
         /**
          * Methods that add an item to the list of inventory items
          */
-        public void AddItem(ItemSO item, int quantity)
+        public int AddItem(ItemSO item, int quantity)
         {
-            for (int i = 0; i < inventoryItems.Count; i++)
+            if (!item.IsStackable)
             {
-                if (inventoryItems[i].IsEmpty)
+                for (int i = 0; i < inventoryItems.Count; i++)
                 {
-                    inventoryItems[i] = new InventoryItem{item = item, quantity = quantity};
-                    return;
+                    while (quantity > 0 && !IsInventoryFull())
+                    {
+                        quantity -= AddNonStackableItem(item, 1);
+                    }
+                    InformAboutChange();
+                    return quantity;
                 }
             }
+
+            quantity = AddStackableItem(item, quantity);
+            InformAboutChange();
+            return quantity;
         }
 
         /**
@@ -50,6 +59,67 @@ namespace Inventory.Model
         public void AddItem(InventoryItem item)
         {
             AddItem(item.item, item.quantity);
+        }
+
+        /**
+         * Methods that add an item that wasn't in the inventory
+         */
+        private int AddNonStackableItem(ItemSO item, int quantity)
+        {
+            InventoryItem newItem = new InventoryItem { item = item, quantity = quantity };
+
+            for (int i = 0; i < inventoryItems.Count; i++)
+            {
+                if (inventoryItems[i].IsEmpty)
+                {
+                    inventoryItems[i] = newItem;
+                    return quantity;
+                }
+            }
+
+            return 0;
+        }
+
+        /**
+         * Methods that add an item that is already in the inventory (by increasing
+         * its quantity)
+         */
+        private int AddStackableItem(ItemSO item, int quantity)
+        {
+            for (int i = 0; i < inventoryItems.Count; i++)
+            {
+                // We check if one of the item in the inventory is the same as the parameter
+                if (inventoryItems[i].IsEmpty) continue;
+                if (inventoryItems[i].item.ID == item.ID)
+                {
+                    // The possible amount of items that can be taken
+                    int amount = inventoryItems[i].item.MaxStackSize - inventoryItems[i].quantity;
+                    
+                    // We increment the quantity according to the capacity
+                    if (quantity > amount) // the maximum has been reached
+                    {
+                        inventoryItems[i] = inventoryItems[i].ChangeQuantity(inventoryItems[i].item.MaxStackSize);
+                        quantity -= amount;
+                        // We doesn't return the quantity in case of having an other item in our inventory
+                    }
+                    else
+                    {
+                        inventoryItems[i] = inventoryItems[i].ChangeQuantity(inventoryItems[i].quantity + quantity);
+                        InformAboutChange();
+                        return 0;
+                    }
+                }
+            }
+
+            // If their is still quantity to add and the inventory has empty places, we add a new item
+            while (quantity > 0 && !IsInventoryFull())
+            {
+                int newQuantity = Mathf.Clamp(quantity, 0, item.MaxStackSize);
+                quantity -= newQuantity;
+                AddNonStackableItem(item, newQuantity);
+            }
+
+            return quantity;
         }
         
         /**
@@ -70,6 +140,11 @@ namespace Inventory.Model
             inventoryItems[index2] = item1;
             InformAboutChange();
         }
+
+        /**
+         * True if the inventory is full, false otherwise
+         */
+        private bool IsInventoryFull() => !inventoryItems.Where(item => item.IsEmpty).Any();
 
         /**
          * Methods that inform that the inventory has changed
